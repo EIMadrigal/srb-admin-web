d2091b1cc83dec449d0e0a7ab47340894d429356
EI Madrigal <andrew_ren@163.com>
2026-01-31 17:33:41 +0800

    display point level list

--- src/views/core/point-level/list.vue
+++ src/views/core/point-level/list.vue
@@ -1,12 +1,35 @@
 <template>
-  <div>
-    Point Level List
+  <div class="app-container">
+    <el-table :data="list" border stripe>
+      <el-table-column type="index" width="50" />
+      <el-table-column prop="borrowAmount" label="Borrow Amount" />
+      <el-table-column prop="pointStart" label="Point Start" />
+      <el-table-column prop="pointEnd" label="Point End" />
+    </el-table>
   </div>
 </template>
 
 <script>
+import pointLevelClient from '@/api/core/point-level'
+
 export default {
-  
+  data() {
+    return {
+      list: []
+    }
+  },
+
+  created() {
+    this.fetchData();
+  },
+
+  methods: {
+    fetchData() {
+      pointLevelClient.list().then(response => {
+        this.list = response.data;
+      })
+    }
+  }
 }
 </script>
 
